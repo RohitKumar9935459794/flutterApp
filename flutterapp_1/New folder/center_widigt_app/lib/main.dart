a8dc9bd7999a3f9d77b43c431f209c02d9c1077e
DELL <rohit307507@gmail.com>
2023-12-30 20:36:04 +0530

    container

---
+++ flutterapp_1/New folder/center_widigt_app/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget build(BuildContext context){
+
+
+    return Scaffold(
+      appBar: AppBar(
+      title:Text('flutter container'),
+      ),
+      body:Center(
+        child:Container(
+        width: 100,
+        height: 100,
+        color: Colors.amber,
+        child: Center(child: Text('hello world',style: TextStyle(color: Colors.black),)),
+      ),
+      )
+    );
+  }
+ 
+  }
+
+ 
